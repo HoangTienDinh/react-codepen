3914eed53a4a41d4f268ca40f83de8369c7b89e4
Hoang Dinh <hoangdinh90@gmail.com>
2020-10-03 22:06:08 -0700

    start layout for comp

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,11 +6,28 @@ import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
 
-const Editor = () => {
+const Editor = ({ displayName, language, value, onChange }) => {
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
   return (
-    <div>
-      EDITOR
-      <div></div>
+    <div className="editor-container">
+      <div className="editor-title">
+        {displayName}
+        <button>O/C</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          lineNumbers: true,
+        }}
+      />
     </div>
   );
 };
