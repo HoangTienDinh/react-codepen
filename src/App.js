e8a99b42dde29d8594cc448c32698b7327822cbf
Hoang Dinh <hoangdinh90@gmail.com>
2020-10-03 22:10:22 -0700

    add state and pass props for css and js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import Editor from "./components/Editor";
 function App() {
   const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+
 
   return (
     <>
       <div className="pane top-pane">
         <Editor language="xml" displayName="HTML" value={html} onChange={setHtml} />
-        <Editor />
-        <Editor />
+        <Editor language="css" displayName="CSS" value={css} onChange={setCss} />
+        <Editor language="javascript" displayName="JS" value={js} onChange={setJs} />
       </div>
       <div className="pane"></div>
       <iframe
