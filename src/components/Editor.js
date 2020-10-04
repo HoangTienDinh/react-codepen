c6153179d33bad5b2732b4bbd203b371604f0abb
Hoang Dinh <hoangdinh90@gmail.com>
2020-10-03 22:36:33 -0700

    change button icon

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -5,6 +5,8 @@ import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Editor = ({ displayName, language, value, onChange }) => {
   const [open, setOpen] = useState(true);
@@ -17,7 +19,9 @@ const Editor = ({ displayName, language, value, onChange }) => {
     <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className="editor-title">
         {displayName}
-        <button onClick={() => setOpen((prevOpen) => !prevOpen)}>O/C</button>
+        <button onClick={() => setOpen((prevOpen) => !prevOpen)}>
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
