1dc7ca2b7e43f1874c57e5ad94166cdfedb26851
Hoang Dinh <hoangdinh90@gmail.com>
2020-10-03 22:21:02 -0700

    add useEffect to use timeout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Editor from "./components/Editor";
 
 function App() {
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      `);
+    }, 250);
+  }, [html, css, js]);
 
   return (
     <>
       <div className="pane top-pane">
-        <Editor language="xml" displayName="HTML" value={html} onChange={setHtml} />
-        <Editor language="css" displayName="CSS" value={css} onChange={setCss} />
-        <Editor language="javascript" displayName="JS" value={js} onChange={setJs} />
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
       </div>
       <div className="pane"></div>
       <iframe
+        srcDoc={srcDoc}
         title="output"
         sandbox="allow-scripts"
         frameBorder="0"
